e65f2d1d52c46e0b15cf813ffb9edde0d41c940e
Greg Gentling <greg@7studios.com>
2025-10-26 19:46:14 -0700

    fixed import

--- src/pages/Documentation.tsx
+++ src/pages/Documentation.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { ChevronDown } from 'lucide-react';
-import toast from 'react-hot-toast';
 
 interface DocumentationItem {
   id: string;
